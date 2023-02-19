5577b79e435669ad3d2bbac140a1a66a6be19b49
Aymen Taouil <aymentaouil123@gmail.com>
2023-02-19 09:45:09 +0100

    Fix request body always null

--- src/main/java/com/aol/backend/MainVerticle.java
+++ src/main/java/com/aol/backend/MainVerticle.java
@@ -3,12 +3,14 @@ package com.aol.backend;
 import com.aol.backend.controllers.TableController;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
 
 public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start(){
     Router router = Router.router(vertx);
+    router.route().handler(BodyHandler.create());
     TableController databaseController = new TableController(vertx);
     router.get("/api/db/hello").handler(ctx ->{
       ctx.request().response().end("Hello");
