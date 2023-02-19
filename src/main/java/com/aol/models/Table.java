f2cdb34efbb1739142783ff83ad743e13b5a0265
Aymen Taouil <aymentaouil123@gmail.com>
2023-02-19 11:20:25 +0100

    Parse column names from json to create table

--- src/main/java/com/aol/models/Table.java
+++ src/main/java/com/aol/models/Table.java
@@ -5,16 +5,20 @@ import java.util.List;
 public class Table {
   private String name;
   private List<Column> columns;
+  private List<String> columnNames;
   private List<Row> rows;
 
-  public Table(String name) {
+  public Table(String name,List<String> columnNames) {
     this.name = name;
+    this.columnNames = columnNames;
   }
 
   public String toString() {
     StringBuilder sb = new StringBuilder();
     sb.append("I'm a table, my current name is ");
     sb.append(name);
+    sb.append("and the columns i'm comprised of are ");
+    sb.append(columnNames);
     return sb.toString();
   }
 }
