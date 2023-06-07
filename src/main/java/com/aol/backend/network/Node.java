2b505a568302a7081479239d08bf65fde08397bd
Ola <ola.hneini@gmail.com>
2023-06-07 18:17:18 +0200

    feat: add node membership listener event

--- src/main/java/com/aol/backend/network/Node.java
+++ src/main/java/com/aol/backend/network/Node.java
@@ -1,6 +1,8 @@
 package com.aol.backend.network;
 import com.aol.backend.MainVerticle;
 import com.aol.backend.Utils.TableSerializableFactory;
+import com.hazelcast.cluster.MembershipEvent;
+import com.hazelcast.cluster.MembershipListener;
 import com.hazelcast.config.Config;
 import com.hazelcast.config.SerializationConfig;
 import com.hazelcast.core.HazelcastInstance;
@@ -32,7 +34,25 @@ public class Node {
         if (clusterManager.getNodes().size() == 1)
           System.out.println("Cluster up and running!");
         else
-          System.out.println("New node joined cluster!");
+          System.out.println("Node successfully joined cluster!");
+
+        // register membership listener to detect when a node joins/leaves
+        HazelcastInstance hazelcastInstance= clusterManager.getHazelcastInstance();
+        hazelcastInstance.getCluster().addMembershipListener(new MembershipListener() {
+          @Override
+          public void memberAdded(MembershipEvent membershipEvent) {
+            // a new node joined the cluster
+            String newMemberAddress = membershipEvent.getMember().getAddress().toString();
+            System.out.println("New node joined the cluster: " + newMemberAddress);
+          }
+
+          @Override
+          public void memberRemoved(MembershipEvent membershipEvent) {
+            // a node left the cluster
+            String leftMemberAddress = membershipEvent.getMember().getAddress().toString();
+            System.out.println("A node left the cluster: " + leftMemberAddress);
+          }
+        });
 
       } else {
           if (clusterManager.getNodes().size() == 1)
